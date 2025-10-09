045e350ffb1d5d5dbde998bc60c5be2dccd175b9
Christophe Amory <christophe@amory.fr>
2025-10-09 14:45:14 +0200

    feat(worker): synchronize serving status with task execution lifecycle

--- armonik-worker/src/main/java/fr/aneo/armonik/worker/TaskOutcome.java
+++ armonik-worker/src/main/java/fr/aneo/armonik/worker/TaskOutcome.java
@@ -1,6 +1,6 @@
 package fr.aneo.armonik.worker;
 
-sealed public interface TaskOutcome {
+public sealed interface TaskOutcome {
   record Success() implements TaskOutcome {}
   record Error(String message) implements TaskOutcome {}
 }
